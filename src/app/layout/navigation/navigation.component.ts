698ad6241a17e1fa2bfc6d82a0188e38aadc3a6c
Daehwa Seo <sdhwa92@gmail.com>
2019-10-10 15:23:42 +1000

    Dynamic navigation bar
    Change navigation bar dynamically based on the page

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { appRoutesNames } from '../../app.routes.names';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+
+  logo = 'assets/img/logo.png';
+  items = [
+    { link: appRoutesNames.HOME, text: 'Home' },
+    { link: appRoutesNames.CLUBS, text: 'Clubs' },
+    { link: appRoutesNames.STATS, text: 'Stats'}
+  ];
 
   constructor(
     private router: Router
@@ -16,7 +25,13 @@ export class NavigationComponent implements OnInit {
   }
 
   activeRoute(routeName: string): boolean {
+    // console.log('route name: ' + routeName);
     return this.router.url.indexOf(routeName) > -1;
+  }
+
+  get currentUrl() {
+    // console.log(this.router.url);
+    return this.router.url;
   }
 
 }
