e617e308b69ae1d7ebe900c6a5990a8dbc87a953
Daehwa Seo <sdhwa92@gmail.com>
2019-10-14 11:35:18 +1000

    Prevent memory leaking

--- src/app/modules/club/page/club.component.ts
+++ src/app/modules/club/page/club.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ClubList } from '@data/schema/club-list';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import * as _ from 'lodash';
 
@@ -11,20 +12,31 @@ import * as _ from 'lodash';
   templateUrl: './club.component.html',
   styleUrls: ['./club.component.css']
 })
-export class ClubComponent implements OnInit {
+export class ClubComponent implements OnInit, OnDestroy {
 
   clubs: ClubList[];
+
+  private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(
     private activatedRoute: ActivatedRoute
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.loadClubs();
   }
 
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
   private loadClubs() {
-    this.activatedRoute.data.subscribe(
+    this.activatedRoute.data
+      .pipe(
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe(
       data => {
         this.clubs = _.orderBy(data.clubsData, ['name'], ['asc']);
         console.log(this.clubs);
--- src/app/modules/stats/page/stats/stats.component.ts
+++ src/app/modules/stats/page/stats/stats.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { PlayerDetails } from '@data/schema/player-details';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css']
 })
-export class StatsComponent implements OnInit, AfterViewInit {
+export class StatsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   public displayedColumns: string[] = ['playerName', 'club', 'nationality', 'goals'];
   public dataSource = new MatTableDataSource<PlayerDetails>();
 
   @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -27,8 +31,17 @@ export class StatsComponent implements OnInit, AfterViewInit {
     this.dataSource.sort = this.sort;
   }
 
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
   private loadAllPlayersStats() {
-    this.activatedRoute.data.subscribe(
+    this.activatedRoute.data
+      .pipe(
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe(
       data => {
         console.log(data.statsData);
         this.dataSource.data = data.statsData as PlayerDetails[];
